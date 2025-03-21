6d63264267ae69c9b5f28a2509a451555d1c670d
Pranav Kr <vanarp022@gmail.com>
2025-03-21 18:00:19 +0530

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,7 +5,7 @@ export const projects: Project[] = [
     id: 1,
     title: "Virtual Interview Suite",
     description: "A next-gen coding interview hub, integrating live video calls, screen sharing, and a multi-language editor",
-    image: "./../../assets/virtualInterviewSuite.png",
+    image: "https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?q=80&w=3455&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     technologies: ["WebDev", "NextJS", "React"],
     githubUrl: "https://github.com/guytesting85/virtual-interview-suite",
     liveUrl: "https://virtual-interview-suite.vercel.app/"
@@ -14,7 +14,7 @@ export const projects: Project[] = [
     id: 2,
     title: "Portfolio Website",
     description: "A website that showcases my projects and work, enabling companies and individuals to evaluate me accordingly.",
-    image: "./../../assets/portfolioWebsite.png",
+    image: "https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?q=80&w=3455&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     technologies: ["WebDev", "JS", "React"],
     githubUrl: "http://localhost:5173/",
     liveUrl: "http://localhost:5173/"
