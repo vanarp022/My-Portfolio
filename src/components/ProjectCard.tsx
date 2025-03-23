ceb20fee452cf6781bf8cd4bee208073cfda5a5d
Pranav Kr <vanarp022@gmail.com>
2025-03-23 16:44:48 +0530

    added link to images

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -22,17 +22,17 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project, featured = false, de
       `}
       style={{ animationDelay }}
     >
-      <div className="relative overflow-hidden group">
       <a href={project.liveUrl} target="_blank">
+      <div className="relative overflow-hidden group">
         <img
           src={project.image}
           alt={project.title}
 
           className="w-full h-48 object-cover transform transition-transform duration-500 group-hover:scale-110"
         />
-        </a>
         <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
       </div>
+        </a>
       
       <div className="p-6">
         <h3 className="text-xl font-bold mb-2 text-gray-900 dark:text-white">{project.title}</h3>
